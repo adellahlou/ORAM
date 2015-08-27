7a479180dccb8679a3cebbb8891268d6289b93f0
Amar Saggu <amar.singh.saggu@gmail.com>
2015-08-27 13:01:25 +0100

    Add BlockStore for manipulating block devices
    Created the abstract class BlockStore to allow for different types of
    storage devices to be used and interchanged (such as a file or some
    allocated memory).

---
+++ src/BlockStore.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Types.hpp"
+
+#include <vector>
+
+// The size of blocks
+constexpr size_t BlockSize = 1024*4; // 4 KiB
+
+using block = bytes<BlockSize>;
+
+/*
+ * An abstract class for interfacing with
+ * various different block devices
+ */
+class BlockStore {
+public:
+	virtual ~BlockStore() = default;
+
+	virtual bytes<BlockSize> Read(size_t pos) = 0;
+	virtual void Write(size_t pos, block b) = 0;
+
+	virtual size_t GetSize() = 0;
+};
